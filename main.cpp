4e4046288b95a7c04959e2f6115dd838e36bb995
hieule <lehieu10a13@gmail.com>
2026-03-31 13:55:57 +0700

    Sua doi ten ham phu hop

--- main.cpp
+++ main.cpp
@@ -43,7 +43,8 @@ inline uint32_t XOR_32bit(uint32_t text1,uint32_t text2)
 {
     return (text1 ^ text2);
 }
-void XOR_text(uint32_t text1[4],uint32_t text2[4],uint32_t result[4])
+
+void AddRoundKey(uint32_t text1[4],uint32_t text2[4],uint32_t result[4])
 {
     for (int i = 0; i < 4; i++)
     {
@@ -54,7 +55,7 @@ void XOR_text(uint32_t text1[4],uint32_t text2[4],uint32_t result[4])
     
 }
  // Ham xu li khoa tung loop
-void AddRoundKey(uint32_t key[4],int i)
+void Expansion_key(uint32_t key[4],int i)
 {
     uint32_t ImKey[4];
     for (int i = 0; i < 4; i++)
@@ -153,7 +154,7 @@ int main()
             keys[j] = static_cast<uint32_t>(stoul(cut_key,nullptr,16));
         }
      
-        XOR_text(words,keys,results);
+        AddRoundKey(words,keys,results);
         in_ketqua(results,"hien thi ket qua khoi tao : ");
         // Thuat toan ma hoa AES
         for (int i = 1; i < 10; i++)
@@ -166,9 +167,9 @@ int main()
             in_ketqua(results,"Ket qua shiftRows: ");
             mixColumns(results);
             in_ketqua(results,"Ket qua mixColumns: ");
-            AddRoundKey(keys,i);
-            in_ketqua(keys,"Ket qua AddRoundkey: ");
-            XOR_text(results,keys,results);
+            Expansion_key(keys,i);
+            in_ketqua(keys,"Ket qua Sau khi mo rong : ");
+            AddRoundKey(results,keys,results);
             in_ketqua(results,"Ket qua lan tiep theo :");
 
 
@@ -180,9 +181,9 @@ int main()
         in_ketqua(results,"Ket qua subBytes: ");
         shiftRows(results);
         in_ketqua(results,"Ket qua shiftRows: ");
-        AddRoundKey(keys,10);
-        in_ketqua(keys,"Ket qua AddRoundkey: ");
-        XOR_text(results,keys,results);
+        Expansion_key(keys,10);
+        in_ketqua(keys,"Ket qua Sau khi mo rong : ");
+        AddRoundKey(results,keys,results);
         // Hien thi ket qua ma hoa
         cout << "\n Thong tin : " << text;
         cout << "\n Khoa      : " << key;
@@ -245,7 +246,7 @@ int main()
         }
         for (int i = 1; i <= 10; i++)
         {
-            AddRoundKey(IM_key,i);
+            Expansion_key(IM_key,i);
             for (int j = 0; j < 4; j++)
             {
             
@@ -263,8 +264,8 @@ int main()
             
         }
         cout << "\nBuoc thu 10 :\n";
-        XOR_text(cipher_text,Khoa_GiaiMa[9],cipher_text);
-        in_ketqua(cipher_text,"Ciphertext sau khi XOR AddRoundKey truoc do: ");
+        AddRoundKey(cipher_text,Khoa_GiaiMa[9],cipher_text);
+        in_ketqua(cipher_text,"Ciphertext sau khi XOR  truoc do: ");
         invShiftRows(cipher_text);
         in_ketqua(cipher_text,"Ciphertext Sau khi invShiftRows truoc do: ");
         invSubBytes(cipher_text);
@@ -272,7 +273,7 @@ int main()
         for (int i = 9; i>0; i--)
         {
             cout << "Buoc thu " << i << ":\n";
-            XOR_text(cipher_text,Khoa_GiaiMa[i-1],cipher_text);
+            AddRoundKey(cipher_text,Khoa_GiaiMa[i-1],cipher_text);
             in_ketqua(cipher_text,"Ciphertext sau khi XOR AddRoundKey truoc do: ");
             invMixColumns(cipher_text);
             in_ketqua(cipher_text,"Ciphertext Sau khi phep MixColumns : ");
@@ -281,7 +282,7 @@ int main()
             invSubBytes(cipher_text);
             in_ketqua(cipher_text,"Ciphertext Sau phep invSubBytes : ");
         }
-        XOR_text(cipher_text,keys,cipher_text);
+        AddRoundKey(cipher_text,keys,cipher_text);
         in_ketqua(keys,"Khoa ban dau : ");
         in_ketqua(cipher_text,"Thong tin sau giai ma: ");
         // Hien thi ket qua ma hoa
