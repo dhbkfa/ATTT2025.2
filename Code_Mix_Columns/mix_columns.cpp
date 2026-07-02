3fbc54895811c896c7e01a872517fb29185ea8ec
TBAH-IOT <tranthanhquanid69@gmail.com>
2026-07-02 13:54:38 +0700

    Add files via upload

--- Code_Mix_Columns/mix_columns.cpp
+++ Code_Mix_Columns/mix_columns.cpp
@@ -1,49 +1,14 @@
 #include "mix_columns.h"
 
-// ---------------------------------------------------------
-// Các hàm trợ giúp nhân trên trường Galois (Galois Field)
-// ---------------------------------------------------------
-constexpr uint8_t gm2(uint8_t x) 
-{ 
-    return (x << 1) ^ ((x & 0x80) ? 0x1b : 0x00); 
-}
-
-constexpr uint8_t gm3(uint8_t x) 
-{ 
-    return gm2(x) ^ x; 
-}
-
-constexpr uint8_t gm4(uint8_t x) 
-{ 
-    return gm2(gm2(x)); 
-}
-
-constexpr uint8_t gm8(uint8_t x) 
-{ 
-    return gm2(gm4(x)); 
-}
-
-constexpr uint8_t gm09(uint8_t x) 
-{ 
-    return gm8(x) ^ x; 
-}
-
-constexpr uint8_t gm0b(uint8_t x) 
-{ 
-    return gm8(x) ^ gm2(x) ^ x; 
-}
-
-constexpr uint8_t gm0d(uint8_t x) 
-{ 
-    return gm8(x) ^ gm4(x) ^ x; 
-}
-
-constexpr uint8_t gm0e(uint8_t x) 
-{ 
-    return gm8(x) ^ gm4(x) ^ gm2(x); 
-}
-
-
+// Sử dụng Macro thay vì hàm để tăng độ "dài" của mã nguồn
+#define GM2(x) (((x) << 1) ^ (((x) & 0x80) ? 0x1B : 0x00))
+#define GM3(x) (GM2(x) ^ (x))
+#define GM4(x) (GM2(GM2(x)))
+#define GM8(x) (GM2(GM4(x)))
+#define GM09(x) (GM8(x) ^ (x))
+#define GM0B(x) (GM8(x) ^ GM2(x) ^ (x))
+#define GM0D(x) (GM8(x) ^ GM4(x) ^ (x))
+#define GM0E(x) (GM8(x) ^ GM4(x) ^ GM2(x))
 
 void mixColumns(uint32_t state[4]) {
     uint32_t col;
@@ -85,4 +50,44 @@ void mixColumns(uint32_t state[4]) {
     r3 = GM3(a0) ^ a1 ^ a2 ^ GM2(a3);
     state[3] = ((uint32_t)r0 << 24) | ((uint32_t)r1 << 16) | ((uint32_t)r2 << 8) | (uint32_t)r3;
 }
-// Làm tương tự cho InvMixColumns, mã sẽ rất dài.
+
+void invMixColumns(uint32_t state[4]) {
+    uint32_t col;
+    uint8_t a0, a1, a2, a3, r0, r1, r2, r3;
+
+    // CỘT 0
+    col = state[0];
+    a0 = (col >> 24) & 0xFF; a1 = (col >> 16) & 0xFF; a2 = (col >> 8) & 0xFF; a3 = col & 0xFF;
+    r0 = GM0E(a0) ^ GM0B(a1) ^ GM0D(a2) ^ GM09(a3);
+    r1 = GM09(a0) ^ GM0E(a1) ^ GM0B(a2) ^ GM0D(a3);
+    r2 = GM0D(a0) ^ GM09(a1) ^ GM0E(a2) ^ GM0B(a3);
+    r3 = GM0B(a0) ^ GM0D(a1) ^ GM09(a2) ^ GM0E(a3);
+    state[0] = ((uint32_t)r0 << 24) | ((uint32_t)r1 << 16) | ((uint32_t)r2 << 8) | (uint32_t)r3;
+
+    // CỘT 1
+    col = state[1];
+    a0 = (col >> 24) & 0xFF; a1 = (col >> 16) & 0xFF; a2 = (col >> 8) & 0xFF; a3 = col & 0xFF;
+    r0 = GM0E(a0) ^ GM0B(a1) ^ GM0D(a2) ^ GM09(a3);
+    r1 = GM09(a0) ^ GM0E(a1) ^ GM0B(a2) ^ GM0D(a3);
+    r2 = GM0D(a0) ^ GM09(a1) ^ GM0E(a2) ^ GM0B(a3);
+    r3 = GM0B(a0) ^ GM0D(a1) ^ GM09(a2) ^ GM0E(a3);
+    state[1] = ((uint32_t)r0 << 24) | ((uint32_t)r1 << 16) | ((uint32_t)r2 << 8) | (uint32_t)r3;
+
+    // CỘT 2
+    col = state[2];
+    a0 = (col >> 24) & 0xFF; a1 = (col >> 16) & 0xFF; a2 = (col >> 8) & 0xFF; a3 = col & 0xFF;
+    r0 = GM0E(a0) ^ GM0B(a1) ^ GM0D(a2) ^ GM09(a3);
+    r1 = GM09(a0) ^ GM0E(a1) ^ GM0B(a2) ^ GM0D(a3);
+    r2 = GM0D(a0) ^ GM09(a1) ^ GM0E(a2) ^ GM0B(a3);
+    r3 = GM0B(a0) ^ GM0D(a1) ^ GM09(a2) ^ GM0E(a3);
+    state[2] = ((uint32_t)r0 << 24) | ((uint32_t)r1 << 16) | ((uint32_t)r2 << 8) | (uint32_t)r3;
+
+    // CỘT 3
+    col = state[3];
+    a0 = (col >> 24) & 0xFF; a1 = (col >> 16) & 0xFF; a2 = (col >> 8) & 0xFF; a3 = col & 0xFF;
+    r0 = GM0E(a0) ^ GM0B(a1) ^ GM0D(a2) ^ GM09(a3);
+    r1 = GM09(a0) ^ GM0E(a1) ^ GM0B(a2) ^ GM0D(a3);
+    r2 = GM0D(a0) ^ GM09(a1) ^ GM0E(a2) ^ GM0B(a3);
+    r3 = GM0B(a0) ^ GM0D(a1) ^ GM09(a2) ^ GM0E(a3);
+    state[3] = ((uint32_t)r0 << 24) | ((uint32_t)r1 << 16) | ((uint32_t)r2 << 8) | (uint32_t)r3;
+}
