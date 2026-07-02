40a45a1e424ab461d5879b0b09a7eed948671943
TBAH-IOT <tranthanhquanid69@gmail.com>
2026-07-02 13:43:51 +0700

    Add files via upload

--- Code_Mix_Columns/mix_columns.h
+++ Code_Mix_Columns/mix_columns.h
@@ -1,9 +1,24 @@
+/**
+ * @file mix_columns.h
+ * @brief Định nghĩa các hàm MixColumns và InvMixColumns cho thuật toán AES.
+ * @version 1.0
+ */
+
 #ifndef MIX_COLUMNS_H
 #define MIX_COLUMNS_H
 
 #include <stdint.h>
 
+/**
+ * @brief Thực hiện phép biến đổi MixColumns trên State.
+ * @param state Mảng 4 phần tử 32-bit đại diện cho State matrix.
+ */
 void mixColumns(uint32_t state[4]);
+
+/**
+ * @brief Thực hiện phép biến đổi InvMixColumns (giải mã) trên State.
+ * @param state Mảng 4 phần tử 32-bit đại diện cho State matrix.
+ */
 void invMixColumns(uint32_t state[4]);
 
 #endif // MIX_COLUMNS_H
